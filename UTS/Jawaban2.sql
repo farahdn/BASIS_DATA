c413afb7d78dfb41eb5473acadb6301ce8cac4d0
farahdina <145762435+farahdn@users.noreply.github.com>
2023-11-16 17:34:27 +0700

    Create Jawaban2.sql

---
+++ UTS/Jawaban2.sql
@@ -0,0 +1,11 @@
+insert into mata_kuliah values
+(1, 'sistem operasi'),
+(2, 'kalkulus');
+
+insert into mahasiswa value
+(1, 'nadia', 'recilia', '20220803212', 1),
+(2, 'gusti', 'putu', '20220801347', 2);
+
+insert into dosen value
+(1, 'jeffry', 'sunupurwa', 'csf309', 1),
+(2, 'badie', 'uddin', 'csf308', 2); 
